37cc81700464f58aa03ffae07a0c83bceed613bc
lisider <1368317399@qq.com>
2018-01-23 21:49:09 +0800

    error

--- config_vim.sh
+++ config_vim.sh
@@ -18,7 +18,7 @@ mv ~/.vim ~/.vim_bak
 mv ~/.vimrc ~/.vim_bak
 
 
-git clone git@github.com:lisider/.vim.git ~/.vim
+git clone https://github.com/lisider/.vim.git ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ln -s ~/.vim/.vimrc ~/.vimrc
